7d8af696df1c7f97a4d1022e38f05a7270f52d7d
Alvarado08 <jorgealvarado0238@gmail.com>
2025-09-07 01:50:33 -0500

    elevently json fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("public");
+  eleventyConfig.addPassthroughCopy("package.json");
   return {
     dir: {
       input: "src",
