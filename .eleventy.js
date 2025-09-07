a4aff846a93aee126fac3e20b75d7455818ad0b3
Alvarado08 <jorgealvarado0238@gmail.com>
2025-09-07 02:08:37 -0500

    elevently json fix api

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("public");
   eleventyConfig.addPassthroughCopy("package.json");
+  eleventyConfig.addPassthroughCopy({
+    "src/_api/package.json": "_api/package.json",
+  });
   return {
     dir: {
       input: "src",
